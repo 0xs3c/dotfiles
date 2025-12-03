db78851998dc21751b37376da84660828b320e4b
0xs3c <s3c.3xc@gmail.com>
2025-12-03 16:54:06 +0300

    changing dmenu theme

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -8,10 +8,10 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+		    /*     fg       bg       */
+	[SchemeNorm] = { "#d6cfcf", "#282A30" },
+	[SchemeSel] = { "#efe9f0", "#464954" },
+	[SchemeOut] = { "#d6cfcf", "#32343C" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
